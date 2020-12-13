13691869f46a4e77a1ec048dda7d152d7307793f
afewok <afewok@hotmail.com>
2020-12-14 02:45:36 +0800

    2020-12-14

--- learn-java/src/test/java/com/afewok/learn/leetcode/LeetCode049.java
+++ learn-java/src/test/java/com/afewok/learn/leetcode/LeetCode049.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.testng.annotations.Test;
 
@@ -12,18 +14,56 @@ public class LeetCode049 {
     @Test
     public void leetCode() {
         Flow.executor(() -> groupAnagrams(new String[] { "eat", "tea", "tan", "ate", "nat", "bat" }));
-        Flow.executor(() -> groupAnagrams(new String[] { "cab", "tin", "pew", "duh", "may", "ill", "buy", "bar", "max", "doc" }));
+        Flow.executor(() -> groupAnagrams(
+                new String[] { "cab", "tin", "pew", "duh", "may", "ill", "buy", "bar", "max", "doc" }));
     }
 
-    public List<List<String>> groupAnagrams(String[] strs) {
+    public List<List<String>> groupAnagrams1(String[] strs) {
         Map<String, List<String>> map = new HashMap<>();
         for (String str : strs) {
-            StringBuilder stringBuilder = new StringBuilder("000000000000000000000000000");
+            int[] temps = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
             for (int i = 0; i < str.length(); i++) {
-                stringBuilder.setCharAt(((int) str.charAt(i) - 97), '1');
+                int temp = ((int) str.charAt(i) - 97);
+                temps[temp] = temps[temp] + 1;
+            }
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int temp : temps) {
+                stringBuilder.append(temp + ".");
             }
 
             List<String> list = map.computeIfAbsent(stringBuilder.toString(), key -> new ArrayList<String>());
+            list.add(str);
+        }
+        return map.values().stream().collect(Collectors.toList());
+    }
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        char[] chs;char c;int length=0;
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            chs = str.toCharArray();
+            length=chs.length;
+            for (int i = 0; i < length; i++) {
+                for (int j = i + 1; j < length; j++) {
+                    if (chs[i] > chs[j]) {
+                        c = chs[i];
+                        chs[i] = chs[j];
+                        chs[j] = c;
+                    }
+                }
+            }
+            List<String> list = map.computeIfAbsent(new String(chs), key -> new ArrayList<String>());
+            list.add(str);
+        }
+        return map.values().stream().collect(Collectors.toList());
+    }
+
+    public List<List<String>> groupAnagrams3(String[] strs) {
+        int length=strs.length;
+        Map<String, List<String>> map = new HashMap<>(length);
+        for (String str : strs) {
+            System.out.println( Stream.of(str.toCharArray()).sorted().collect(Collectors.toList()));
+            List<String> list = map.computeIfAbsent( Stream.of(str.toCharArray()).sorted().toString(), key -> new ArrayList<String>());
             list.add(str);
         }
         return map.values().stream().collect(Collectors.toList());
