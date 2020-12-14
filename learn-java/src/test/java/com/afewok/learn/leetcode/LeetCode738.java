2426b119a7cbb9d8e2c92364da2f6aad9a86c468
afewok <afewok@hotmail.com>
2020-12-15 00:19:05 +0800

    2020-12-15

---
+++ learn-java/src/test/java/com/afewok/learn/leetcode/LeetCode738.java
@@ -0,0 +1,21 @@
+package com.afewok.learn.leetcode;
+
+import org.testng.annotations.Test;
+
+/**
+ * 738. 单调递增的数字
+ * 
+ */
+public class LeetCode738 {
+
+    @Test
+    public void leetCode() {
+        Flow.executor(() -> monotoneIncreasingDigits(10));
+        Flow.executor(() -> monotoneIncreasingDigits(1234));
+        Flow.executor(() -> monotoneIncreasingDigits(332));
+    }
+
+    public int monotoneIncreasingDigits(int N) {
+     
+    }
+}
