aa17503536acda074b136dc6950bc6ef8ba15649
afewok <afewok@hotmail.com>
2020-12-17 00:04:09 +0800

    2020-12-17

--- learn-golang/test/leetcode/leetcode_048_test.go
+++ learn-golang/test/leetcode/leetcode_048_test.go
@@ -11,8 +11,15 @@ import (
  * 思路：
  */
 func Test_leetcode_048(t *testing.T) {
-	data1 := [][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}
-	data2 := [][]int{{5, 1, 9, 11}, {2, 4, 8, 10}, {13, 3, 6, 7}, {15, 14, 12, 16}}
+	data1 := [][]int{
+		{1, 2, 3},
+		{4, 5, 6},
+		{7, 8, 9}}
+	data2 := [][]int{
+		{5, 1, 9, 11},
+		{2, 4, 8, 10},
+		{13, 3, 6, 7},
+		{15, 14, 12, 16}}
 	rotate(data1)
 	rotate(data2)
 	fmt.Println(data1)
@@ -22,7 +29,25 @@ func Test_leetcode_048(t *testing.T) {
 func rotate(matrix [][]int) {
 	defer timeCost()()
 	length := len(matrix)
-	for i := 0; i < length; i++ {
+	x1, y1, x2, y2, temp := 0, 0, length-1, length-1, 0
+	for x1 < x2 && y1 < y2 {
+		temp = matrix[y1][x1]
+		matrix[y1][x1] = matrix[y2][x1]
+		matrix[y2][x1] = matrix[y2][x2]
+		matrix[y2][x2] = matrix[y1][x2]
+		matrix[y1][x2] = temp
+		if x1 < x2 {
+			x1++
+		} else {
+			y1++
+			x1 = y1
+		}
 
+		if y1 < y2 {
+			y2--
+		} else {
+			x2--
+			y2 = x2
+		}
 	}
 }
---
+++ learn-golang/test/leetcode/leetcode_714_test.go
@@ -0,0 +1,17 @@
+package leetcode
+
+import (
+	"testing"
+)
+
+/**
+ * 714. 买卖股票的最佳时机含手续费
+ *
+ * 思路：
+ */
+func Test_leetcode_714(t *testing.T) {
+}
+
+func maxProfit(prices []int, fee int) int {
+
+}
