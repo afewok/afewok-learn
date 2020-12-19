3a4ecf10a8e94e855d208b8718df4ff34e732835
afewok <afewok@hotmail.com>
2020-12-19 11:06:34 +0800

    2020-12-19

--- learn-golang/test/leetcode/leetcode_048_test.go
+++ learn-golang/test/leetcode/leetcode_048_test.go
@@ -15,39 +15,32 @@ func Test_leetcode_048(t *testing.T) {
 		{1, 2, 3},
 		{4, 5, 6},
 		{7, 8, 9}}
+	rotate(data1)
+	fmt.Println(data1)
 	data2 := [][]int{
 		{5, 1, 9, 11},
 		{2, 4, 8, 10},
 		{13, 3, 6, 7},
 		{15, 14, 12, 16}}
-	rotate(data1)
 	rotate(data2)
-	fmt.Println(data1)
 	fmt.Println(data2)
 }
 
 func rotate(matrix [][]int) {
 	defer timeCost()()
 	length := len(matrix)
-	x1, y1, x2, y2, temp := 0, 0, length-1, length-1, 0
-	for x1 < x2 && y1 < y2 {
-		temp = matrix[y1][x1]
-		matrix[y1][x1] = matrix[y2][x1]
-		matrix[y2][x1] = matrix[y2][x2]
-		matrix[y2][x2] = matrix[y1][x2]
-		matrix[y1][x2] = temp
-		if x1 < x2 {
-			x1++
+	x, y, max, half, temp := 0, 0, length-1, length/2, 0
+	for y < half {
+		temp = matrix[y][x]
+		matrix[y][x] = matrix[max-x][y]
+		matrix[max-x][y] = matrix[max-y][max-x]
+		matrix[max-y][max-x] = matrix[x][max-y]
+		matrix[x][max-y] = temp
+		if x < max-y-1 {
+			x++
 		} else {
-			y1++
-			x1 = y1
-		}
-
-		if y1 < y2 {
-			y2--
-		} else {
-			x2--
-			y2 = x2
+			y++
+			x = y
 		}
 	}
 }
