bf2910a9499f5db2a76f4595d7ab6932eb13ba7b
afewok <afewok@hotmail.com>
2020-12-20 19:54:10 +0800

    2020-12-20

--- learn-golang/test/leetcode/leetcode_316_test.go
+++ learn-golang/test/leetcode/leetcode_316_test.go
@@ -11,8 +11,31 @@ import (
 
 func Test_leetcode_316(t *testing.T) {
 	fmt.Println(removeDuplicateLetters("bcabc"))
+	fmt.Println(removeDuplicateLetters("cbacdcbc"))
 }
 
 func removeDuplicateLetters(s string) string {
-	defer timeCost()()
+	left := [26]int{}
+	for _, ch := range s {
+		left[ch-'a']++
+	}
+	stack := []byte{}
+	inStack := [26]bool{}
+	for i := range s {
+		ch := s[i]
+		if !inStack[ch-'a'] {
+			for len(stack) > 0 && ch < stack[len(stack)-1] {
+				last := stack[len(stack)-1] - 'a'
+				if left[last] == 0 {
+					break
+				}
+				stack = stack[:len(stack)-1]
+				inStack[last] = false
+			}
+			stack = append(stack, ch)
+			inStack[ch-'a'] = true
+		}
+		left[ch-'a']--
+	}
+	return string(stack)
 }
