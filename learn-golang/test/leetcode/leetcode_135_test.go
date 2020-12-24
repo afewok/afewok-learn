95a2ca572143f52169c954a355c2ec3c4229c857
afewok <afewok@hotmail.com>
2020-12-24 11:29:11 +0800

    2020-12-24

--- learn-golang/test/leetcode/leetcode_135_test.go
+++ learn-golang/test/leetcode/leetcode_135_test.go
@@ -10,22 +10,21 @@ import (
  */
 
 func Test_leetcode_135(t *testing.T) {
-	// fmt.Println(candy([]int{1, 0, 2}))
-	// fmt.Println(candy([]int{1, 2, 2}))
-	// fmt.Println(candy([]int{1, 3, 2, 2, 1}))
-	// fmt.Println(candy([]int{1, 2, 87, 87, 87, 2, 1}))
-	// fmt.Println(candy([]int{29, 51, 87, 87, 72, 12}))
-	// fmt.Println(candy([]int{1, 6, 10, 8, 7, 3, 2}))
+	fmt.Println(candy([]int{1, 0, 2}))
+	fmt.Println(candy([]int{1, 2, 2}))
+	fmt.Println(candy([]int{1, 3, 2, 2, 1}))
+	fmt.Println(candy([]int{1, 2, 87, 87, 87, 2, 1}))
+	fmt.Println(candy([]int{29, 51, 87, 87, 72, 12}))
+	fmt.Println(candy([]int{1, 6, 10, 8, 7, 3, 2}))
 	fmt.Println(candy([]int{1, 3, 4, 5, 2}))
 }
 
-// 5+1+2+3
 func candy(ratings []int) int {
 	defer timeCost()()
-	length, award, i := len(ratings), 0, 1
+	length := len(ratings)
 	arr := make([]int, length)
 
-	for i < length {
+	for i := 1; i < length; i++ {
 		if ratings[i-1] > ratings[i] {
 			j := i + 1
 			for j < length {
@@ -37,24 +36,84 @@ func candy(ratings []int) int {
 			count := 1
 			for k := j - 2; k >= i; k-- {
 				arr[k] = count
-				award += arr[k]
 				count++
 			}
 			if i-1 == 0 || ratings[i-1] == ratings[i-2] {
 				arr[i-1] = count
-				award += arr[i-1]
-			} else if ratings[i-1] > ratings[i-2] {
-				if count > arr[i-1] {
-					award += count - arr[i-1]
-				}
+			} else if ratings[i-1] > ratings[i-2] && count > arr[i-1] {
 				arr[i-1] = count
 			}
 			i = j - 1
 		} else if ratings[i-1] < ratings[i] {
 			arr[i] = arr[i-1] + 1
-			award += arr[i]
 		}
-		i++
+	}
+	for _, v := range arr {
+		length += v
+	}
+	return length
+}
+
+func candy1(ratings []int) int {
+	defer timeCost()()
+	length := len(ratings)
+	arr := make([]int, length)
+
+	for i := 1; i < length; i++ {
+		if ratings[i-1] > ratings[i] {
+			j := i + 1
+			for j < length {
+				if ratings[j-1] <= ratings[j] {
+					break
+				}
+				j++
+			}
+			count := 1
+			for k := j - 2; k >= i; k-- {
+				arr[k] = count
+				count++
+			}
+			if i-1 == 0 || ratings[i-1] == ratings[i-2] {
+				arr[i-1] = count
+			} else if ratings[i-1] > ratings[i-2] && count > arr[i-1] {
+				arr[i-1] = count
+			}
+			i = j - 1
+		} else if ratings[i-1] < ratings[i] {
+			arr[i] = arr[i-1] + 1
+		}
+	}
+	for _, v := range arr {
+		length += v
+	}
+	return length
+}
+
+func candy2(ratings []int) int {
+	defer timeCost()()
+	length, award, sub := len(ratings), 0, -1
+	arr := make([]int, length)
+	for i := 1; i < length; i++ {
+		if ratings[i-1] < ratings[i] {
+			arr[i] = arr[i-1] + 1
+			award += arr[i]
+			sub = -1
+		} else if ratings[i-1] > ratings[i] {
+			if sub == -1 {
+				sub = i - 1
+			} else if sub+1 < i {
+				for j := i - 1; j > sub; j-- {
+					arr[j]++
+					award++
+				}
+			}
+			if arr[sub] == arr[sub+1] {
+				arr[sub]++
+				award++
+			}
+		} else {
+			sub = -1
+		}
 	}
 	return length + award
 }
