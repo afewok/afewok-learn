248efe22dadb2d291b474b4396b602099d959c94
afewok <afewok@hotmail.com>
2021-01-09 00:08:05 +0800

    2021-01-09

--- learn-nodejs/test/leetcode-ts/leetcode_189.test.ts
+++ learn-nodejs/test/leetcode-ts/leetcode_189.test.ts
@@ -1,4 +1,4 @@
-function rotate(nums: number[], k: number): number[] {
+function rotate1(nums: number[], k: number): number[] {
     let length: number = nums.length, num: number = 0
     if (length < k) {
         k = k % length
@@ -14,10 +14,36 @@ function rotate(nums: number[], k: number): number[] {
     return nums
 };
 
+function rotate(nums: number[], k: number): number[] {
+    let length: number = nums.length
+    if (length < k) {
+        k = k % length
+    }
+    let sub: number = 0, num: number = nums[0], count: number = 0
+    let p: number = sub, q: number = p - k < 0 ? length + p - k : p - k
+    while (count < length) {
+        count++
+        if (q == sub) {
+            nums[p] = num
+            sub = q + 1 == length ? 0 : q + 1
+            num = nums[sub]
+            p = sub
+        } else {
+            nums[p] = nums[q]
+            p = q
+        }
+        q = p - k
+        q = q < 0 ? length + q : q
+    }
+    return nums
+}
+
 /**
  * 189. 旋转数组
  */
 test('leetcode_189', () => {
+    console.log(rotate([1], 0))
     console.log(rotate([1, 2, 3, 4, 5, 6, 7], 3))
+    console.log(rotate([-1, -100, 3, 99], 2))
     console.log(rotate([1, 2, 3, 4, 5, 6, 7], 15))
 });
