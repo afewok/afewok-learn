35328a359f64317e9d59fe4b2dc32ac00533fc91
afewok <afewok@hotmail.com>
2021-01-16 20:06:31 +0800

    2020-01-16

--- learn-golang/test/leetcode/leetcode_459_test.go
+++ learn-golang/test/leetcode/leetcode_459_test.go
@@ -10,11 +10,32 @@ import (
  */
 
 func Test_leetcode_459(t *testing.T) {
+	fmt.Println(repeatedSubstringPattern("bb"))
+	fmt.Println(repeatedSubstringPattern("abac"))
 	fmt.Println(repeatedSubstringPattern("abab"))
+	fmt.Println(repeatedSubstringPattern("aba"))
+	fmt.Println(repeatedSubstringPattern("abcabcabcabc"))
 }
 
 func repeatedSubstringPattern(s string) bool {
 	defer timeCost()()
+	length, count, pattern := len(s), 2, false
+	for length >= count {
+		if length%count == 0 {
+			step := length / count
+			temp := s[0:step]
+			pattern = true
+			for i := 2; i <= count; i++ {
+				if temp != s[step*(i-1):step*i] {
+					pattern = false
+					break
+				}
+			}
+			if pattern {
+				return pattern
+			}
+		}
+		count++
+	}
 	return false
-
 }
