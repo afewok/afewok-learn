30090357bcd146e6ee681e56737c1bc41f2a2ec0
afewok <afewok@hotmail.com>
2021-01-22 13:17:52 +0800

    2020-01-22

--- learn-golang/test/leetcode/leetcode_520_test.go
+++ learn-golang/test/leetcode/leetcode_520_test.go
@@ -12,6 +12,7 @@ import (
 func Test_leetcode_520(t *testing.T) {
 	fmt.Println(detectCapitalUse("USA"))
 	fmt.Println(detectCapitalUse("FlaG"))
+	fmt.Println(detectCapitalUse("ffffffffffffffffffffF"))
 }
 
 func detectCapitalUse(word string) bool {
@@ -21,29 +22,28 @@ func detectCapitalUse(word string) bool {
 		return false
 	}
 
-	if word[0]>&&word[0]<{
+	if word[0] >= 97 && word[0] <= 122 {
 		for i := 1; i < length; i++ {
-			if word[0]>&&word[0]<{
+			if word[i] < 97 || word[i] > 122 {
 				return false
 			}
 		}
-	}else{
-		if length < 2 {
-			upper:=true
-			if word[1]>&&word[1]<{
-				upper:=false
+	} else {
+		if length > 2 {
+			upper := true
+			if word[1] >= 97 && word[1] <= 122 {
+				upper = false
 			}
-			for i := 1; i < length; i++ {
-				if word[0]>&&word[0]<{
-					if upper{
-					return false
+			for i := 2; i < length; i++ {
+				if word[i] >= 97 && word[i] <= 122 {
+					if upper {
+						return false
 					}
-				}else if !upper{
+				} else if !upper {
 					return false
-					}
+				}
 			}
 		}
 	}
-
 	return true
 }
--- learn-golang/test/leetcode/leetcode_541_test.go
+++ learn-golang/test/leetcode/leetcode_541_test.go
@@ -10,12 +10,32 @@ import (
  */
 
 func Test_leetcode_541(t *testing.T) {
-	fmt.Println(fib(2))
-	fmt.Println(fib(3))
-	fmt.Println(fib(4))
+	fmt.Println(reverseStr("abcdefg", 2))
 }
 
 func reverseStr(s string, k int) string {
 	defer timeCost()()
+	length, result := len(s), ""
+	direction, count := false, (length+k-1)/k
+	for i := 0; i < count; i++ {
+		s1, s2 := i*k, (i+1)*k
+		if s2 > length {
+			s2 = length
+		}
+		if direction {
+			result += s[s1:s2]
+		} else {
+			result += reverseString541(s[s1:s2])
+		}
+		direction = !direction
+	}
+	return result
+}
 
+func reverseString541(s string) string {
+	runes := []rune(s)
+	for from, to := 0, len(runes)-1; from < to; from, to = from+1, to-1 {
+		runes[from], runes[to] = runes[to], runes[from]
+	}
+	return string(runes)
 }
