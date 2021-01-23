c380105460c441c39161dc9338f01a4833ea0668
afewok <afewok@hotmail.com>
2021-01-24 01:15:16 +0800

    2021-01-24

--- learn-golang/test/leetcode/leetcode_035_test.go
+++ learn-golang/test/leetcode/leetcode_035_test.go
@@ -10,13 +10,13 @@ import (
  */
 
 func Test_leetcode_035(t *testing.T) {
-	fmt.Println(searchInsert([]int{1, 3, 5, 6}, 5))
+	// fmt.Println(searchInsert([]int{1, 3, 5, 6}, 5))
 	fmt.Println(searchInsert([]int{1, 3, 5, 6}, 2))
-	fmt.Println(searchInsert([]int{1, 3, 5, 6}, 7))
-	fmt.Println(searchInsert([]int{1, 3, 5, 6}, 0))
+	// fmt.Println(searchInsert([]int{1, 3, 5, 6}, 7))
+	// fmt.Println(searchInsert([]int{1, 3, 5, 6}, 0))
 }
 
-func searchInsert(nums []int, target int) int {
+func searchInsert1(nums []int, target int) int {
 	defer timeCost()()
 	for i, num := range nums {
 		if num == target || target < num {
@@ -26,20 +26,24 @@ func searchInsert(nums []int, target int) int {
 	return len(nums)
 }
 
-// func searchInsert(nums []int, target int) int {
-// 	defer timeCost()()
-// 	left, right := 0, len(nums)-1
-// 	mid := left + (right-left)/2 + 1
-// 	for left <= right {
-// 		if nums[mid] == target {
-// 			return mid
-// 		} else if nums[mid] > target {
-// 			left = mid
-// 			mid = left + (right-left)/2 + 1
-// 		} else if nums[mid] < target {
-// 			right = mid
-// 			mid = left + (right-left)/2 + 1
-// 		}
-// 	}
+func searchInsert(nums []int, target int) int {
+	defer timeCost()()
+	left, right := 0, len(nums)-1
+	if target < nums[0] {
+		return 0
+	} else if target > nums[right] {
+		return right + 1
+	}
 
-// }
+	for left <= right {
+		mid := left + (right-left+1)/2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] < target {
+			left = mid + 1
+		} else if nums[mid] > target {
+			right = mid - 1
+		}
+	}
+	return left
+}
