a142a03cc51f53124274bc86503c7a80473e2210
afewok <afewok@hotmail.com>
2021-02-02 23:37:14 +0800

    2021-02-02

--- learn-golang/test/leetcode/leetcode_836_test.go
+++ learn-golang/test/leetcode/leetcode_836_test.go
@@ -15,14 +15,14 @@ func Test_leetcode_836(t *testing.T) {
 	fmt.Println(isRectangleOverlap([]int{0, 0, 1, 1}, []int{2, 2, 3, 3}))
 }
 
+// x1<x2
+// y1<y2
 func isRectangleOverlap(rec1 []int, rec2 []int) bool {
 	defer timeCost()()
-	range:=func()bool{
-
+	if rec1[0] >= rec2[2] || rec2[0] >= rec1[2] || rec1[1] >= rec2[3] || rec2[1] >= rec1[3] {
+		return false
+	} else if rec1[0] == rec1[2] || rec1[1] == rec1[3] || rec2[0] == rec2[2] || rec2[1] == rec2[3] {
+		return false
 	}
-
-	if(range()||range()||range()){
-		return true
-	}
-	return false
+	return true
 }
