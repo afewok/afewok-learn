f4d262256484963b97a3ae35e3ec8f89852f56a1
afewok <afewok@hotmail.com>
2021-02-07 01:04:16 +0800

    2021-02-07

--- learn-golang/test/leetcode/leetcode_665_test.go
+++ learn-golang/test/leetcode/leetcode_665_test.go
@@ -16,5 +16,25 @@ func Test_leetcode_665(t *testing.T) {
 
 func checkPossibility(nums []int) bool {
 	defer timeCost()()
-
+	changeNum := 0
+	for i := 1; i <= len(nums)-3; i++ {
+		if nums[i] > nums[i+1] {
+			//removable discontinuity
+			if nums[i-1] <= nums[i+1] {
+				changeNum++
+			} else if nums[i] <= nums[i+2] {
+				changeNum++
+			} else {
+				//jump discontinuity
+				return false
+			}
+		}
+	}
+	if len(nums) >= 2 && nums[0] > nums[1] {
+		changeNum++
+	}
+	if len(nums) >= 3 && nums[len(nums)-2] > nums[len(nums)-1] {
+		changeNum++
+	}
+	return changeNum == 0 || changeNum == 1
 }
