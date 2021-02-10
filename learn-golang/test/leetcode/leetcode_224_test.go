996e766bc6a77a74408a355c3926e8c07d7309c3
afewok <afewok@hotmail.com>
2021-02-10 09:19:30 +0800

    2021-02-10

--- learn-golang/test/leetcode/leetcode_224_test.go
+++ learn-golang/test/leetcode/leetcode_224_test.go
@@ -2,7 +2,6 @@ package leetcode
 
 import (
 	"fmt"
-	"strconv"
 	"testing"
 )
 
@@ -11,41 +10,80 @@ import (
  */
 
 func Test_leetcode_224(t *testing.T) {
-	fmt.Println(calculate("1 + 1"))
-	fmt.Println(calculate(" 2-1 + 2 "))
+	fmt.Println(calculate("1-(5)"))
+	fmt.Println(calculate("- (3 + (4 + 5))"))
+	// fmt.Println(calculate("1 + 1"))
+	// fmt.Println(calculate(" 2-1 + 2 "))
 	fmt.Println(calculate("(1+(4+5+2)-3)+(6+8)"))
 }
 
 func calculate(s string) int {
 	defer timeCost()()
-	temp, stack, result := "", make([]int, 0), 0
+	f := func(temp int, sign rune) int {
+		if sign == '+' {
+			return temp
+		}
+		return -temp
+
+	}
+	temp, sign, stack1, stack2, result := 0, '+', make([]int, 0), make([]rune, 0), 0
 	for _, c := range s {
 		if c == ' ' {
 			continue
 		} else if c >= '0' && c <= '9' {
-			temp = string(append([]rune(temp), c))
-		} else if c == '(' {
-			stack = append(stack, result)
-			result = 0
-		}
+			temp = temp*10 + int(c) - 48
+		} else {
+			result += f(temp, sign)
+			temp = 0
 
-		switch c {
-		case ' ':
-		case '(':
+			if c == '(' {
+				stack1 = append(stack1, result)
+				result = 0
 
-		case ')':
-			length := len(stack) - 1
-			result += stack[length]
-			stack = stack[0:length]
-		case '+':
-			n, _ := strconv.Atoi(temp)
-			result += n
-		case '-':
-			n, _ := strconv.Atoi(temp)
-			result -= n
-		default:
+				stack2 = append(stack2, sign)
+				sign = '+'
+			} else if c == ')' {
+				len1 := len(stack1) - 1
+				len2 := len(stack2) - 1
 
+				result = stack1[len1] + f(result, stack2[len2])
+				sign = '+'
+
+				stack1 = stack1[0:len1]
+				stack2 = stack2[0:len2]
+			} else {
+				sign = c
+			}
 		}
 	}
-	return result
+
+	return result + f(temp, sign)
 }
+
+// if c == '+' || c == '-' {
+// 	sign=c
+// } else if c >= '0' && c <= '9' {
+
+// } else if c == '(' {
+// 	stack = append(stack, result)
+// 	result = 0
+// } else if c == ')' {
+// }
+
+// switch c {
+// case ' ':
+// case '(':
+
+// case ')':
+// 	length := len(stack) - 1
+// 	result += stack[length]
+// 	stack = stack[0:length]
+// case '+':
+// 	n, _ := strconv.Atoi(temp)
+
+// case '-':
+// 	n, _ := strconv.Atoi(temp)
+
+// default:
+
+// }
