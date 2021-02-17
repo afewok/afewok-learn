bf8da8ba8f31b5feb2ecc5d7d1d5c71babe09447
afewok <afewok@hotmail.com>
2021-02-17 11:34:08 +0800

    2020-02-17

--- learn-golang/test/leetcode/leetcode_offer_038_test.go
+++ learn-golang/test/leetcode/leetcode_offer_038_test.go
@@ -7,12 +7,13 @@ import (
 
 /**
  * 剑指 Offer 38. 字符串的排列
+ * 思路：深度优先遍历（递归+回溯+剪枝）、广度优先遍历（动态规划（杨辉三角第n行））
  */
 func Test_leetcode_offer_038(t *testing.T) {
 	fmt.Println(permutation("abc"))
 }
 
-func permutation(s string) []string {
+func permutation1(s string) []string {
 	defer timeCost()()
 	length := len(s)
 	mp, pre, result := make(map[string]int), make([]string, 0), []string{string(s[0])}
@@ -30,4 +31,26 @@ func permutation(s string) []string {
 		result, pre = pre, make([]string, 0)
 	}
 	return result
+}
+
+func permutation(s string) []string {
+	defer timeCost()()
+	result := make([]string, 0)
+	getPermutation(s, string(s[0]), 1, len(s), make(map[string]int), &result)
+	return result
+
+}
+
+func getPermutation(s, temp string, sub, length int, mp map[string]int, result *[]string) {
+	if sub == length {
+		if _, OK := mp[temp]; !OK {
+			mp[temp]++
+			*result = append(*result, temp)
+		}
+		return
+	}
+
+	for j := 0; j <= len(temp); j++ {
+		getPermutation(s, temp[:j]+string(s[sub])+temp[j:], sub+1, length, mp, result)
+	}
 }
