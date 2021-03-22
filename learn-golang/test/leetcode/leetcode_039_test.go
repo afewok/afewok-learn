dc3a032f16d469855f4d464271398b7890f10f4c
afewok <afewok@hotmail.com>
2021-03-22 09:26:41 +0800

    2021-03-22

--- learn-golang/test/leetcode/leetcode_039_test.go
+++ learn-golang/test/leetcode/leetcode_039_test.go
@@ -10,17 +10,25 @@ import (
  */
 
 func Test_leetcode_039(t *testing.T) {
-	fmt.Println(climbStairs(1))
+	fmt.Println(combinationSum([]int{2, 3, 6, 7}, 7))
+	fmt.Println(combinationSum([]int{2, 3, 5}, 8))
 }
 
 func combinationSum(candidates []int, target int) [][]int {
 	defer timeCost()()
-	// mp := make(map[int]bool)
-	// for _, candidate := range candidates {
-	// 	mp[candidate] = true
-	// }
-	// for i := 1; i < target; i++ {
+	mp, length, result := make(map[int]bool), len(candidates), make([][]int, 0)
+	for i := 0; i < length; i++ {
+		mp[candidates[i]] = true
+	}
+	for i := 1; i <= target && i <= length; i++ {
+		temp := getCombinationSum(mp, i, target)
+		if temp != nil {
+			result = append(result, temp...)
+		}
+	}
+	return result
+}
 
-	// }
-	return nil
+func getCombinationSum(mp map[int]bool, count, target int) [][]int {
+
 }
---
+++ learn-golang/test/leetcode/leetcode_093_test.go
@@ -0,0 +1,26 @@
+package leetcode
+
+import (
+	"fmt"
+	"testing"
+)
+
+/**
+ * 93. 复原 IP 地址
+ */
+
+func Test_leetcode_093(t *testing.T) {
+	fmt.Println(climbStairs(1))
+}
+
+func restoreIpAddresses(s string) []string {
+	defer timeCost()()
+	// mp := make(map[int]bool)
+	// for _, candidate := range candidates {
+	// 	mp[candidate] = true
+	// }
+	// for i := 1; i < target; i++ {
+
+	// }
+	return nil
+}
